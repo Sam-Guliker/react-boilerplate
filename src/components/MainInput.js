3f208306f4486d9333c39ad1e84ea6038bb51aad
Senmetsu <samguliker@hotmail.com>
2018-07-20 21:04:00 +0200

    fetch

---
+++ src/components/MainInput.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/css/index.css';
+
+class MainInput extends Component {
+  render() {
+    return (
+        <div className='container'>
+        <h1>Testing</h1>
+        </div>
+    );
+  }
+}
+
+export default MainInput;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
+import MainInput from '../components/MainInput.js'
 import logo from '../logo.svg';
 import '../styles/css/index.css';
 
 class App extends Component {
+
+  initialState() {
+    quote: ''
+    authorName: ''
+  }
+
+  componentDidMount() {
+    const that = this
+    const url =  'https://talaikis.com/api/quotes/random'
+
+    fetch(url).then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log('No data !'))
+  }
+
+  renderData(data) {
+    console.log(data)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main>
+        <MainInput/>
+      </main>
     );
   }
 }
